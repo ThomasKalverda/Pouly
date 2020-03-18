069a86d4e72bcfb6ca2317f4570c3417f2ce7085
Thomas <thomas_kalverda@hotmail.com>
2020-03-18 17:25:40 +0100

    model migrations
    lobby has poules in database

--- lobby/admin.py
+++ lobby/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Poule, Sport
 
+admin.site.register(Poule)
+admin.site.register(Sport)
 # Register your models here.
--- lobby/models.py
+++ lobby/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Sport(models.Model):
+    name = models.CharField(max_length=100, default='Voetbal')
+    imageURL = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Poule(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=300, blank=True)
+    imageURL = models.CharField(max_length=100, blank=True)
+    sport = models.ForeignKey(Sport, on_delete=models.SET_NULL, null=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
