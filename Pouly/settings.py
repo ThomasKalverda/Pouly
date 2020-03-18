1088409bbc2cd699a99dba4f1530976ee86e255c
Thomas <thomas_kalverda@hotmail.com>
2020-03-18 19:16:21 +0100

    Creating register form
    using templates to create the form for Django

--- Pouly/settings.py
+++ Pouly/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'lobby.apps.LobbyConfig',
     'poule.apps.PouleConfig',
     'django.contrib.admin',
