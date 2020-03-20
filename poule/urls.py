44c4c780e9626febf2e3c264f2947499390ff62e
Thomas <thomas_kalverda@hotmail.com>
2020-03-20 17:28:31 +0100

    basic UI changes

--- poule/urls.py
+++ poule/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='poule-home'),
+    path('', views.overview, name='poule-overview'),
+    path('ranking/', views.ranking, name='poule-ranking'),
+    path('predictions/', views.predictions, name='poule-predictions'),
+    path('teams/', views.teams, name='poule-teams'),
+    path('info/', views.info, name='poule-info'),
+    path('games/', views.games, name='poule-games'),
+    path('rules/', views.rules, name='poule-rules'),
 ]
--- poule/views.py
+++ poule/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'poule/home.html')
+
+def overview(request):
+    return render(request, 'poule/overview.html', {'sbar': 'overview'})
+
+
+def ranking(request):
+    return render(request, 'poule/ranking.html', {'sbar': 'ranking'})
+
+
+def predictions(request):
+    return render(request, 'poule/predictions.html', {'sbar': 'predictions'})
+
+
+def games(request):
+    return render(request, 'poule/games.html', {'sbar': 'games'})
+
+
+def rules(request):
+    return render(request, 'poule/rules.html', {'sbar': 'rules'})
+
+
+def teams(request):
+    return render(request, 'poule/teams.html', {'sbar': 'teams'})
+
+
+def info(request):
+    return render(request, 'poule/info.html', {'sbar': 'info'})
